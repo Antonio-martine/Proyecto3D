25d85752d88ce0fcf702e4fe20de8cace41a8f0b
Antonio-martine <jesus07antonio03@gmail.com>
2022-11-15 22:40:19 -0600

    Version 1.2 3D Project

--- rutaEjs/user.js
+++ rutaEjs/user.js
@@ -5,8 +5,17 @@ const ruta = express.Router();
 /*Primera pantalla*/
 ruta.get('/',(req,res)=>{
     res.render('index');
-})
+});
 
+/*Iniciar Sesión*/
+ruta.get('/iniciarSesion',(req,res)=>{
+    res.render('iniciarSesion');
+});
+
+/*CrearSesion*/
+ruta.get('/crearSesion',(req,res)=>{
+    res.render('crearSesion');
+});
 
 /*Exportacion*/
 module.exports=ruta;
